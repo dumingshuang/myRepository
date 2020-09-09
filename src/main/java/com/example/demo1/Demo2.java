ea8950c1fc8c6056ecc49eff5557611c89291067
dumingshuang <2317119620@qq.com>
2020-09-09 09:30:17 +0800

    测试提交2

---
+++ src/main/java/com/example/demo1/Demo2.java
@@ -0,0 +1,12 @@
+package com.example.demo1;
+
+/**
+ *
+ * @Author: dumingshuang
+ * @Date: 2020/9/9 9:29
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+        System.out.println("垃圾");
+    }
+}
