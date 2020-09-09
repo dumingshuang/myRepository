b451a73a6477ad97a4593b25ab4a2adb9f4c0b38
dumingshuang <2317119620@qq.com>
2020-09-09 09:26:21 +0800

    测试提交

---
+++ src/main/java/com/example/demo1/demo.java
@@ -0,0 +1,12 @@
+package com.example.demo1;
+
+/**
+ *
+ * @Author: dumingshuang
+ * @Date: 2020/9/9 9:23
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("垃圾");
+    }
+}
